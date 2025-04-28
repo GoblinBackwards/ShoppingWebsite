97a7f55cef49ec6a4a3dd54248344ec9a1eb7e6e
GoblinBackwards <22856555+GoblinBackwards@users.noreply.github.com>
2025-04-28 16:17:24 +0100

    Improve storefront layout using flex.

--- ShoppingWebsiteMvc/Models/StoreItem.cs
+++ ShoppingWebsiteMvc/Models/StoreItem.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShoppingWebsiteMvc.Models
 {
@@ -8,6 +9,7 @@ namespace ShoppingWebsiteMvc.Models
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
+        [DisplayName("Price")]
         public required decimal PriceGBP { get; set; }
     }
 }
